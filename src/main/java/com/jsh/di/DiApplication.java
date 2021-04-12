7e3de989b6448dd15cbe66f50476df91f3270049
suhojang <jsh2808@naver.com>
2021-04-12 13:15:01 +0900

    Initial commit

---
+++ src/main/java/com/jsh/di/DiApplication.java
@@ -0,0 +1,13 @@
+package com.jsh.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiApplication.class, args);
+	}
+
+}
