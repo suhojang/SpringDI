f5d0dfe0ed194140cb89690675f077a80226ab4c
suhojang <jsh2808@naver.com>
2021-04-12 14:26:11 +0900

    Component -> Repository modify

--- src/main/java/com/jsh/di/repository/SpringDIRepository.java
+++ src/main/java/com/jsh/di/repository/SpringDIRepository.java
@@ -2,7 +2,6 @@ package com.jsh.di.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
