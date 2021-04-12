b05fc110906a370b3a0f1cadc887eb08729701af
suhojang <jsh2808@naver.com>
2021-04-12 14:25:38 +0900

    README.md Commit

--- src/main/java/com/jsh/di/repository/SpringDIRepository.java
+++ src/main/java/com/jsh/di/repository/SpringDIRepository.java
@@ -3,8 +3,9 @@ package com.jsh.di.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class SpringDIRepository {
     private Logger logger   = LoggerFactory.getLogger(SpringDIRepository.class);
 
